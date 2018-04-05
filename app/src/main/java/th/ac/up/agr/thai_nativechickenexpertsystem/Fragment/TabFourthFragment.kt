fda414317fbb0dcc41a6874e1ee3c5aea808d2b3
melondev <59022222@live.up.ac.th>
2018-04-06 01:27:31 +0700

    ุ6 APR 2018

--- app/src/main/java/th/ac/up/agr/thai_nativechickenexpertsystem/Fragment/TabFourthFragment.kt
+++ app/src/main/java/th/ac/up/agr/thai_nativechickenexpertsystem/Fragment/TabFourthFragment.kt
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_tab_fourth.view.*
+import th.ac.up.agr.thai_nativechickenexpertsystem.Adapter.FeaturesListVerticalAdapter
 
 import th.ac.up.agr.thai_nativechickenexpertsystem.R
+import th.ac.up.agr.thai_nativechickenexpertsystem.Tools.QuickRecyclerView
 
 
 /**
@@ -18,6 +21,35 @@ class TabFourthFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_tab_fourth, container, false)
 
+
+        val data = ArrayList<String>()
+        data.add("โรคนิวคลาสเซิล")
+        data.add("โรคฝีดาษ")
+        data.add("โรคอหิวาต์ไก่")
+        data.add("โรคหลอดลมอักเสบ")
+        data.add("โรคนิวคลาสเซิล")
+        data.add("โรคฝีดาษ")
+        data.add("โรคอหิวาต์ไก่")
+        data.add("โรคหลอดลมอักเสบ")
+        data.add("โรคนิวคลาสเซิล")
+        data.add("โรคฝีดาษ")
+        data.add("โรคอหิวาต์ไก่")
+        data.add("โรคหลอดลมอักเสบ")
+
+        val recyclerView = QuickRecyclerView(context!!
+                , view.disease_recycler_view
+                , "linear"
+                , 1
+                , "vertical"
+                , false
+                , "never"
+                , "high")
+                .recyclerView()
+
+        //val FirebaseFunction = FirebaseOnFunction(view.context)
+
+        recyclerView.adapter = FeaturesListVerticalAdapter(view.context, data)
+        //FirebaseFunction.toRecyclerView(FirebaseFunction.databaseReference.child("ลักษณะ").child(string), recyclerView, view.second_loading)
 
 
         return view
